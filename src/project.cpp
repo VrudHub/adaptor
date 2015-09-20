71bde751d80890e56df65b320abf5ae125d1498e
RLad95 <raj95_lad@Hotmail.com>
2015-09-19 20:19:18 -0400

    project
    version1

--- src/project.cpp
+++ src/project.cpp
@@ -1,1 +1,77 @@
 #include "project.hpp"
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+#include <string>
+
+
+using namespace std;
+
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+    
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        cout << "SDL could not initialize! SDL Error: %s\n"<< SDL_GetError();
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            cout<< "Window could not be created! SDL Error: %s\n" << SDL_GetError() ;
+            success = false;
+        }
+        else
+        {
+            //Initialize PNG loading
+            int imgFlags = IMG_INIT_PNG;
+            if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            {
+                cout<<  "SDL_image could not initialize! SDL_image Error: %s\n"<< IMG_GetError();
+                success = false;
+            }
+            else
+            {
+                //Get window surface
+                gScreenSurface = SDL_GetWindowSurface( gWindow );
+            }
+        }
+    }
+    
+    return success;
+}
+
+SDL_Surface* loadSurface( std::string path )
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+    
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        cout<< "Unable to load image %s! SDL_image Error: %s\n", path.c_str() << IMG_GetError();
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, NULL );
+        if( optimizedSurface == NULL )
+        {
+            cout<<  "Unable to optimize image %s! SDL Error: %s\n", path.c_str() << SDL_GetError() ;
+        }
+        
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    
+    return optimizedSurface;
+}
