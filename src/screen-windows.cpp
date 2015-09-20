d9e5ef03eb7ad6d2711561402ec09dafa0adfb03
noryb009 <noryb009@gmail.com>
2015-09-20 03:10:29 -0400

    Add screen-windows

--- src/screen-windows.cpp
+++ src/screen-windows.cpp
@@ -4,5 +4,41 @@
 
 #include "screen-windows.hpp"
 
+Mat* screen_windows::capture() = {
+    HDC screen = GetDC(GetDesktopWindow());
+    int w = GetDeviceCaps(screen, HORZRES);
+    int h = GetDeviceCaps(screen, VERTRES);
+
+    HDC mem = CreateCompatibleDC(screen);
+    HBITMAP b = CreateCompatibleBitmap(scren, w, h);
+    HGDIOBJ obj = SelectObject(mem, b);
+    BitBlt(mem, 0, 0, w, h, screen, 0, 0, SRCCOPY);
+    SelectObject(mem, obj);
+
+    BITMAPINFOHEADER info = {0};
+    info.biSize = sizeof(BITMAPINFOHEADER);
+    info.biPlanes = 1;
+    info.biBitCount = 32;
+    info.biWidth = w;
+    info.biHeight = -h;
+    info.biCompression = BI_RGB;
+    info.biSizeImage = 0;
+
+    uchar* data = new uchar[4 * w * h]();
+
+    GetDIBits(mem, b, 0, h, data, (BITMAPINFO*)&info, DIB_RGB_COLORS);
+
+    ReleaseDC(GetDesktopWindow(), screen);
+    DeleteDC(mem);
+    DeleteObject(b);
+
+    for(long i = 0; i < h * w * 4; i += 4) {
+        uchar tmp = data[i];
+        data[i] = data[i+2];
+        data[i+2] = tmp;
+    }
+
+    return Mat(h, w, CV_8UC4, data);
+}
 
 #endif
--- src/screen-windows.hpp
+++ src/screen-windows.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
 
 class screen_windows : screen {
     virtual Mat* capture() = 0;
